1ce4d3809324651e95676b9fe60a5e3bd600380b
Marcelo Teixeira <marcelo.t.martins@hotmail.com>
2021-09-03 14:26:26 -0300

    aterado paginas About.html(razor) e HomeController

--- VendasWebMvc/Controllers/HomeController.cs
+++ VendasWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,10 @@ namespace VendasWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Vendas Web MVC do curso C#";
+            ViewData["Professor"] = "Nelio";
+            ViewData["Aluno"] = "Marcelo Teixeira Martins";
+            ViewData["email"] = "marcelo.t.martins@hotmail.com";
 
             return View();
         }
